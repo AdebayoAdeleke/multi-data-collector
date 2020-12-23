48feaf20e34ebf128b85d6ff3ff7d0a5d6150100
Adebayo Adeleke <>
2020-12-23 19:50:33 +0100

    Initial commit

---
+++ src/main/java/com/exquisappfactory/multidatacollector/MultiDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.exquisappfactory.multidatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDataCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exquisappfactory/multidatacollector/MultiDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exquisappfactory.multidatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
